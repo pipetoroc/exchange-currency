a2ef7d2d000784b4f4e50fface77073b1f288d45
Felipe Toro C <ftorocastilla@gmail.com>
2024-07-06 12:37:31 -0300

    feat: maquetacao do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
     <>
+      <header>
+        <nav>
+          <ul>
+            <a href=''><img src='./assets/logo3.svg' alt='logo do aplicativo CUCO' /></a>
+            <a href=''> <li>Converter</li></a>
+            <a href=''> <li>FAQ</li></a>
+            <a href=''> <li>Contact</li></a>
+          </ul>
+        </nav>
+      </header>
+      <h1>Conversão de Moedas com Facilidade</h1>
+      <a href=''>Comprar</a>
+      <a href=''>Vender</a>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src='./assets/logo.svg' alt='logo moeda' />
+        <input type='text' placeholder='Digite o nome da moeda' />
+        <p placeholder='0000'>-</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src='./assets/logo2.svg' alt='logo botao de converter' />
     </>
   )
 }
